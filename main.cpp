e3d138bb5d0bc004bc1aefd3e8268a6217e4e462
dawidg81 <dawidgorski.m@gmail.com>
2026-04-11 12:33:20 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <raylib.h>
+#include ""
+
+int main (void) {
+    const int windowHeight = 720;
+    const int windowWidth = 1280;
+
+    InitWindow(windowWidth, windowHeight, "Pong");
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()){
+        BeginDrawing();
+
+        ClearBackground(WHITE);
+
+	DrawCircle(windowWidth / 2, windowHeight / 2, windowHeight / 2, BLACK);
+	DrawRectangle(windowWidth / 2, windowHeight / 2, windowWidth, windowHeight, RED);
+	DrawLine(0, windowHeight, windowWidth, 0, GREEN);
+	DrawLine(0, 0, windowWidth, windowHeight, BLUE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
